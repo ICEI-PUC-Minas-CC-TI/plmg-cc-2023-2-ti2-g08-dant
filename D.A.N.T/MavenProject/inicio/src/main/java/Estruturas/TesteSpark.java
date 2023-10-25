c94d0634315e0f0019a2b3a739481b844acf45dd
sn0ttz <dpmaia0503@gmail.com>
2023-10-24 22:08:33 -0300

    Testando...

--- D.A.N.T/MavenProject/inicio/src/main/java/Estruturas/TesteSpark.java
+++ D.A.N.T/MavenProject/inicio/src/main/java/Estruturas/TesteSpark.java
@@ -2,22 +2,24 @@ package Estruturas;
 
 import static spark.Spark.*;
 
+import Service.UserService;
+
 import com.google.gson.Gson;
 
 import Estruturas.DAOStruct.UserDAO;
 
 public class TesteSpark {
     public static void main(String[] args) {
-        UserDAO teste = new UserDAO();
-        path("/teste", () ->{
+        UserService service = new UserService();
+        path("/teste", () -> {
             get("/hello", (req, res) -> "Sexo infinito");
             post("/", (request, response) -> {
                 String user = request.queryParams("user");
                 System.out.println(user);
                 String senha = request.queryParams("senha");
                 System.out.println(senha);
-                teste.authentication(user,senha);   
-                return 0;
+                int res = service.auth(request, response);
+                return res;
             });
         });
     }
